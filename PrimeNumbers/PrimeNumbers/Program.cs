b3d5b1bd9ffc9d6714efb821ff08431fa20f5634
ianb33 <ianberry33@icloud.com>
2022-09-15 10:41:13 -0400

    Complete?

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace PrimeNumbers
+﻿using System.Globalization;
+
+namespace PrimeNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for(int i = 2; i < 7920; i++)
+            {
+                bool isPrime = true;
+                for(int j = 2; j < i ; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                    if (isPrime)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
         }
     }
 }
